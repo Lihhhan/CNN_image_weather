8f1edcb9c9c02386e93e01d41bc5459c7dfa33ee
lihhhhhan <lh_dlut@163.com>
2016-12-06 12:48:50 +0800

    add

---
+++ SVM.sh
@@ -0,0 +1,25 @@
+python SVM.py  feature/train_fc8.txt feature/test_fc8.txt
+
+python SVM.py  feature/train_fc7.txt feature/test_fc7.txt
+
+python SVM.py  feature/train_fc6.txt feature/test_fc6.txt
+
+python SVM.py  feature/train_pool5.txt feature/test_pool5.txt
+
+python SVM.py  feature/train_conv5.txt feature/test_conv5.txt
+
+python SVM.py  feature/train_conv4.txt feature/test_conv4.txt
+
+python SVM.py  feature/train_conv3.txt feature/test_conv3.txt
+
+python SVM.py  feature/train_pool2.txt feature/test_pool2.txt
+
+python SVM.py  feature/train_pool1.txt feature/test_pool1.txt
+
+
+
+
+
+
+
+
---
+++ get_feature.sh
@@ -0,0 +1,22 @@
+#python get_feature.py data/weather/validation.txt fc6 test
+
+python get_feature.py data/weather/train.txt pool5 train
+python get_feature.py data/weather/validation.txt pool5 test
+
+python get_feature.py data/weather/train.txt conv5 train
+python get_feature.py data/weather/validation.txt conv5 test
+
+python get_feature.py data/weather/train.txt conv4 train
+python get_feature.py data/weather/validation.txt conv4 test
+
+python get_feature.py data/weather/train.txt conv3 train
+python get_feature.py data/weather/validation.txt conv3 test
+
+python get_feature.py data/weather/train.txt pool1 train
+python get_feature.py data/weather/validation.txt pool1 test
+
+python get_feature.py data/weather/train.txt pool2 train
+python get_feature.py data/weather/validation.txt pool2 test
+
+
+
